3f12f13ffd18baad68aec88e326444723beac64d
L8rlTinkerer <nickumcb@gmail.com>
2020-09-07 14:24:49 +1200

    Added GetPostById and refactored posts generation into PostService

--- DotNetCore3.1RestAPI/Controllers/V1/PostsController.cs
+++ DotNetCore3.1RestAPI/Controllers/V1/PostsController.cs
@@ -1,6 +1,9 @@
 ﻿
 using DotNetCore3._1RestAPI.Contracts.V1;
+using DotNetCore3._1RestAPI.Contracts.V1.Requests;
+using DotNetCore3._1RestAPI.Contracts.V1.Responses;
 using DotNetCore3._1RestAPI.Domain;
+using DotNetCore3._1RestAPI.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,21 +14,47 @@ namespace DotNetCore3._1RestAPI.Controllers
 {
     public class PostsController : Controller
     {
-        private List<Post> _posts;
-
-        public PostsController()
+        private readonly IPostService _postService;
+        public PostsController(IPostService postService)
         {
-            _posts = new List<Post>();
-            for (var i= 0; i<5; i++)
-            {
-                _posts.Add(new Post { Id = Guid.NewGuid().ToString() });
-            }
+            _postService = postService;
         }
 
         [HttpGet(ApiRoutes.Posts.GetAll)]
         public IActionResult GetAll()
         {
-            return Ok(_posts);
+            return Ok(_postService.GetPosts());
         }
+
+        [HttpGet(ApiRoutes.Posts.Get)]
+        public IActionResult Get([FromRoute] Guid postId)
+        {
+            var post = _postService.GetPostById(postId);
+
+            if (post == null)
+                return NotFound();
+
+            return Ok(post);
+        }
+
+
+        [HttpPost(ApiRoutes.Posts.Create)]
+        public IActionResult Create([FromBody] CreatePostRequest postRequest)
+        {
+            var post = new Post { Id = postRequest.Id };
+
+            if (post.Id != Guid.Empty)
+                post.Id = Guid.NewGuid();
+
+            _postService.GetPosts().Add(post);
+
+            var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.ToUriComponent()}";
+            var locationUrl = baseUrl + "/" + ApiRoutes.Posts.Get.Replace("postId", post.Id.ToString());
+
+            var response = new PostResponse { Id = post.Id };
+
+            return Created(locationUrl, response);
+        }
+
     }
 }
